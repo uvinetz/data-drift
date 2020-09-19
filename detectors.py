2c0edad8b403bb27e347e9763c86565b02891a21
uvinetz <uvinetz@gmail.com>
2020-09-19 16:56:33 +0300

    First commit, basic definitions

--- detectors.py
+++ detectors.py
@@ -1,9 +1,25 @@
 import pandas as pd
+import numpy as np
+from scipy.stats import mannwhitneyu, ks_2samp
 
 
 class BaseDetector:
-    def __init__(self, param):
-        self._param = param
+    def compare_two_numerical_distributions(
+        self,
+        x1: np.ndarray,
+        x2: np.ndarray,
+        test: str = "mw",
+        significance: float = 0.05,
+    ):
+        self._validate_test_name(test)
+        test_dict = dict(mw=mannwhitneyu, ks=ks_2samp)
+        statistic, p_value = test_dict[test](x1, x2, alternative="two_sided")
+        return p_value < significance
+
+    @staticmethod
+    def _validate_test_name(test):
+        if test not in ["mw", "ks"]:
+            raise ValueError("Not a valid test name")
 
 
 class TimeBasedDetector(BaseDetector):
